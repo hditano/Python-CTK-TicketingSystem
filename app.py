a4d9edca16104328fc58ec7b436e9dfec2b83474
Hernan Di Tano <hditano@gmail.com>
2023-08-22 00:12:27 +0200

    added peewee and basic create database and add data to it

--- app.py
+++ app.py
@@ -1,40 +1,25 @@
 from datetime import datetime
 import customtkinter
+from peewee import *
 
-class CompanyModel:
-    
-    name: str
-    address: str
-    phone_number: str
-    email_address: str
-    city: str
-    country: str
-    
-    def __init__(self, Name: str, Address: str, Phone_Number: str, Email_Address: str, City: str, Country: str) -> None:
-        self.name = Name
-        self.address = Address
-        self.phone_number = Phone_Number
-        self.email_address = Email_Address
-        self.city = City
-        self.country = Country
 
+db = SqliteDatabase('database.db')
+
+class Company(Model):
+    
+    name: str = CharField()
+    address: str = CharField()
+    phone_number: str = CharField()
+    email_address: str = CharField()
+    city: str = CharField()
+    country: str = CharField()
+    
+    class Meta:
+        database = db
+    
 class Ticket:
     
-    id: int
-    
-    def __init__(self, ID) -> None:
-        self.id = ID
-    
-    def initID(self):
-        now = datetime.now()
-        myId = now.strftime('%H%M%S') * 2
-        return myId
-    
-    def new_ticket(self, name, address, phone_number, email_address, city, country):
-        newTicket: CompanyModel = CompanyModel(name, address, phone_number, email_address, city, country)
-        return print(newTicket)
-    
-    def run_app(self):
+    def new_ticket(self):
         print('Hello to Ticketing App')
         name = input('Type Name: ')
         address = input('Type address: ')
@@ -42,8 +27,9 @@ class Ticket:
         email_address = input('Type email address: ')
         city = input('Type City: ')
         country = input('Type Country: ')
-        self.new_ticket(name, address, phone_number, email_address, city, country)
-
+        new_ticket = Company.create(name=name, address=address, phone_number=phone_number, email_address=email_address, city=city, country=country)
+        new_ticket.save()
+        
 class App(customtkinter.CTk):
     def __init__(self) -> None:
         super().__init__()
@@ -52,12 +38,22 @@ class App(customtkinter.CTk):
         self.geometry('800x600')
         self.grid_columnconfigure((0,1), weight=1)
         
-        self.button = customtkinter.CTkButton(self, text='Save', command=self.button_callback)
-        self.button.grid(row=0, column=0, pady=20, sticky='ew', columnspan=2)
+        self.button1 = customtkinter.CTkButton(self, text='Create Database', command=self.button_create_database)
+        self.button1.grid(row=0, column=0, pady=20, sticky='ew', columnspan=2)
+        self.button2 = customtkinter.CTkButton(self, text='Insert Data', command=self.add_data)
+        self.button2.grid(row=1, column=0, pady=20, sticky='ew', columnspan=2)
     
-    def button_callback(self):
-        print('button pressed')
+    def button_create_database(self):
+        database = db
+        print('Connecting to Database..')
+        db.connect()
+        print('Creating Tables')
+        db.create_tables([Company])
+        print('Tables Created')
     
+    def add_data(self):
+        myTicket = Ticket()
+        myTicket.new_ticket()   
 
 if __name__ == '__main__':
     app = App()
